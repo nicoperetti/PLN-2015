351ca0c8a8307a59e41d97af6f7017c299d7cf38
nico <nicoperetti.16@gmail.com>
2015-10-07 00:26:34 -0300

    baseline andando. Pasan los test

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,4 +1,4 @@
-
+from collections import defaultdict
 
 class BaselineTagger:
 
@@ -6,7 +6,24 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        dict_t = defaultdict(dict)
+        tag_frec = defaultdict(int)
+        for tagged_sent in tagged_sents:
+            for k, v in tagged_sent:
+                tag_frec[v] += 1
+                try:
+                    dict_t[k][v] += 1
+                except KeyError:
+                    dict_t[k][v] = 1
+
+# busco el tag mas frecuente
+        m = sorted(tag_frec.items(), key = lambda sor: -sor[1])
+        self.tag_more_frec = m[0][0]
+# busco los tags mas frecuentes para cada palabra
+        self.tag_dict = defaultdict(str)
+        for k, v in dict_t.items():
+            parcial_v = sorted(v.items(), key = lambda sor: -sor[1])
+            self.tag_dict[k] = parcial_v[0][0]
 
     def tag(self, sent):
         """Tag a sentence.
@@ -17,14 +34,20 @@ class BaselineTagger:
 
     def tag_word(self, w):
         """Tag a word.
-
         w -- the word.
         """
-        return 'nc'
+        if self.unknown(w):
+            tag = self.tag_more_frec
+        else:
+            tag = self.tag_dict[w]
+        return tag
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
-
         w -- the word.
         """
-        return True
+        result = False
+        word = self.tag_dict[w]
+        if word == "":
+            result = True
+        return result
