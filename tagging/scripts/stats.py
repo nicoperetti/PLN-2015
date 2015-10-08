63338e4fa37895d53492e88ab625293118ad788b
nico <nicoperetti.16@gmail.com>
2015-10-08 11:58:47 -0300

    Ejercicio1: primeras estadisticas listas, faltan todavia

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -10,7 +10,7 @@ Options:
 from docopt import docopt
 
 from corpus.ancora import SimpleAncoraCorpusReader
-
+from collections import defaultdict
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
@@ -20,4 +20,22 @@ if __name__ == '__main__':
     sents = list(corpus.tagged_sents())
 
     # compute the statistics
+    words = defaultdict(int)
+    tags = defaultdict(int)
+    long_sent = 0
+    for sent in sents:
+        l += len(sent)
+        for pair in sent:
+            words[pair[0]] += 1
+            tags[pair[1]] += 1
+
+    tags_sorted = sorted(tags.items(), key = lambda sor: -sor[1])
+    tags_sorted = tags_sorted[:10]
+    tags_ocur = long_sent
     print('sents: {}'.format(len(sents)))
+    print('word_ocur: {}'.format(long_sent)
+    print('voc_words_size: {}'.format(len(words)))
+    print('voc_tags_size: {}'.format(len(tags)))
+    print('tags_frec: {}'.format()
+
+
