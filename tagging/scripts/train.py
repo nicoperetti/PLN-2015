a9896b921a93949279ef59abd01c394d728ad77b
nico <nicoperetti.16@gmail.com>
2015-10-16 21:11:34 -0300

    train acepta modelo addone

--- tagging/scripts/train.py
+++ tagging/scripts/train.py
@@ -1,12 +1,14 @@
 """Train a sequence tagger.
 
 Usage:
-  train.py [-m <model>] -o <file>
+  train.py [-m <model>] [-n <n>] -o <file>
   train.py -h | --help
 
 Options:
   -m <model>    Model to use [default: base]:
                   base: Baseline
+                  addone: Addone
+  -n <n>        Order of the model[only if the model is not base]
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
@@ -15,10 +17,11 @@ import pickle
 
 from corpus.ancora import SimpleAncoraCorpusReader
 from tagging.baseline import BaselineTagger
-
+from tagging.hmm import MLHMM
 
 models = {
     'base': BaselineTagger,
+    'addone':MLHMM,
 }
 
 
@@ -31,7 +34,12 @@ if __name__ == '__main__':
     sents = list(corpus.tagged_sents())
 
     # train the model
-    model = models[opts['-m']](sents)
+    m = opts['-m']
+    if m == 'addone':
+        n = int(opts['-n'])
+        model = models[m](n, sents)
+    else:
+        model = models[m](sents)
 
     # save it
     filename = opts['-o']
