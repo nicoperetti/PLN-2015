e6d40d52c349ab6a57ed79a8ed887f6386d18968
nico <nicoperetti.16@gmail.com>
2015-11-09 17:27:30 -0300

    Mejoras del cky y modificaciones en upcfg

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -10,16 +10,28 @@ class CKYParser:
         """
         produc = grammar.productions()
         self.start = repr(grammar.start())
-        self.N = []
-        self.lexical_produc = []
-        self.nonlexical_produc = []
+        self.lexical_produc = defaultdict(list)
+        self.nonlexical_produc = defaultdict(list)
 
         for prod in produc:
-            self.N.append(repr(prod.lhs()))
             if prod.is_lexical():
-                self.lexical_produc.append(prod)
+                x_i = prod.rhs()[0]
+                self.lexical_produc[x_i].append(prod)
             else:
-                self.nonlexical_produc.append(prod)
+                Y, Z = self.get_param(prod, 'right')
+                self.nonlexical_produc[(Y, Z)].append(prod)
+
+    def get_param(self, production, option):
+        if option == 'left':
+            result = str(production.lhs())
+        elif option == 'right':
+            p = production.rhs()
+            Y = str(p[0])
+            Z = str(p[1])
+            result = (Y, Z)
+        else:
+            result = production.logprob()
+        return result
 
     def parse(self, sent):
         """Parse a sequence of terminals.
@@ -28,52 +40,46 @@ class CKYParser:
         n = len(sent)
         self._pi = defaultdict(dict)
         self._bp = defaultdict(dict)
+        lp = 0.0
+        t = None
 
         # initialization
         for i, w in enumerate(sent, 1):
-            for production in self.lexical_produc:
-                if production.rhs()[0] == w:
-                    key = (i, i)
-                    non_terminal = repr(production.lhs())
-                    prob = production.logprob()
-                    self._pi[key][non_terminal] = prob
-                    tree = Tree(non_terminal, [w])
-                    self._bp[key][non_terminal] = tree
+            for production in self.lexical_produc[w]:
+                key = (i, i)
+                non_terminal = self.get_param(production, 'left')
+                prob = self.get_param(production, 'prob')
+                self._pi[key][non_terminal] = prob
+                tree = Tree(non_terminal, [w])
+                self._bp[key][non_terminal] = tree
 
         # cky
         for l in range(1, n):
             for i in range(1, (n-l) + 1):
                 j = i + l
-                for production in self.nonlexical_produc:
-                    for s in range(i, j):
-                        Y, Z = production.rhs()
-                        Y = repr(Y)
-                        Z = repr(Z)
-                        pi_1_ant = self._pi[(i, s)]
-                        pi_2_ant = self._pi[(s+1, j)]
-#                        print("---¿?No hace falta pero no pasa los test¿?---")
-                        izq = self._bp[(i, s)]
-                        der = self._bp[(s+1, j)]
-#                        print("-----------¿?¿?------------------------------")
-                        if Y in pi_1_ant and Z in pi_2_ant:
-                            prob = production.logprob()
-                            prob += self._pi[(i, s)][Y] + self._pi[(s+1, j)][Z]
-                            X = repr(production.lhs())
-                            key = (i, j)
-                            izq = self._bp[(i, s)][Y]
-                            der = self._bp[(s+1, j)][Z]
-                            tree = Tree(X, [izq, der])
-                            if X in self._pi[key]:
-                                if prob > self._pi[key][X]:
+                key = (i, j)
+                for s in range(i, j):
+                    pi_1_ant = self._pi[(i, s)]
+                    pi_2_ant = self._pi[(s+1, j)]
+                    izq = self._bp[(i, s)]
+                    der = self._bp[(s+1, j)]
+                    for Y, prob_pi_1 in pi_1_ant.items():
+                        for Z, prob_pi_2 in pi_2_ant.items():
+                            prod = self.nonlexical_produc[(Y, Z)]
+                            for production in prod:
+                                X = self.get_param(production, 'left')
+                                prob = self.get_param(production, 'prob')
+                                prob += prob_pi_1 + prob_pi_2
+                                if X in self._pi[key]:
+                                    if prob > self._pi[key][X]:
+                                        self._pi[key][X] = prob
+                                        tree = Tree(X, [izq[Y], der[Z]])
+                                        self._bp[key][X] = tree
+                                else:
                                     self._pi[key][X] = prob
+                                    tree = Tree(X, [izq[Y], der[Z]])
                                     self._bp[key][X] = tree
-                            else:
-                                self._pi[key][X] = prob
-                                self._bp[key][X] = tree
         if self.start in self._pi[(1, n)]:
             lp = self._pi[(1, n)][self.start]
             t = self._bp[(1, n)][self.start]
-        else:
-            lp = 0.0
-            t = None
         return (lp, t)
--- parsing/upcfg.py
+++ parsing/upcfg.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from parsing.util import unlexicalize, lexicalize
-from nltk.grammar import Nonterminal as N, ProbabilisticProduction, induce_pcfg
+from nltk.grammar import Nonterminal as N, ProbabilisticProduction, PCFG
 from parsing.cky_parser import CKYParser
 from nltk.tree import Tree
 
@@ -9,40 +9,27 @@ class UPCFG:
     """Unlexicalized PCFG.
     """
 
-    def __init__(self, parsed_sents, start='S'):
+    def __init__(self, parsed_sents, start='sentence'):
         """
         parsed_sents -- list of training trees.
         """
-        production = []
         produc_count = defaultdict(int)
         produc_left_count = defaultdict(int)
-
-# DOIT pasar a normal_form
 
         for parsed_sent in parsed_sents:
             uparsed_sent = parsed_sent.copy(deep=True)
             uparsed_sent = unlexicalize(uparsed_sent)
+            uparsed_sent.chomsky_normal_form()
+            uparsed_sent.collapse_unary(collapsePOS=True, collapseRoot=True)
             prod = uparsed_sent.productions()
             for p in prod:
-                X = str(p.lhs())
-                tupl = (X,)
-                for i in p.rhs():
-                    tupl += (str(i),)
-                produc_count[tupl] += 1
-                produc_left_count[X] += 1
-
-        for key, value in produc_count.items():
-            X = key[0]
-            Y = key[1]
-            if len(key) < 3:
-                p = produc_count[(X, Y)] / float(produc_left_count[X])
-                pp = ProbabilisticProduction(N(X), [Y], prob=p)
-            else:
-                Z = key[2]
-                p = produc_count[(X, Y, Z)] / float(produc_left_count[X])
-                pp = ProbabilisticProduction(N(X), [N(Y), N(Z)], prob=p)
-            production += [pp]
-        self.grammar = induce_pcfg(N(start), production)
+                produc_count[p] += 1
+                produc_left_count[p.lhs()] += 1
+        production = [ProbabilisticProduction(p.lhs(), p.rhs(),
+                      prob=produc_count[p]/float(produc_left_count[p.lhs()]))
+                      for p in produc_count]
+        self.grammar = PCFG(N(start), production)
+        self.parser = CKYParser(self.grammar)
 
     def productions(self):
         """Returns the list of UPCFG probabilistic productions.
@@ -53,14 +40,14 @@ class UPCFG:
         """Parse a tagged sentence.
         tagged_sent -- the tagged sentence (a list of pairs (word, tag)).
         """
-        parser = CKYParser(self.grammar)
+        parser = self.parser
         sent, pos_tag = zip(*tagged_sent)
-        pos_tag = list(pos_tag)
-        prob, tree = parser.parse(pos_tag)
+        prob, tree = parser.parse(list(pos_tag))
         if tree is None:
             start = self.grammar.start()
             leaves = [Tree(tag, [word]) for word, tag in tagged_sent]
             result = Tree(repr(start), leaves)
         else:
             result = lexicalize(tree, sent)
+            result.un_chomsky_normal_form()
         return result
